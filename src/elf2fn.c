cf82ed3128027f9d43d571fa31afd3f5759a56d2
olikraus <olikraus@gmail.com>
2025-01-09 22:42:57 +0100

    elf2fn

---
+++ src/elf2fn.c
@@ -0,0 +1,38 @@
+/*
+        {
+            "section_index":[34, "0x00000022"],
+            "sh_name": ".symtab",
+            "sh_type":[2, "SHT_SYMTAB", "Symbol table"],
+            "sh_flags":[0, []],
+            "sh_addr":[0, "0x00000000"],
+            "sh_offset":[77616, "0x00012f30"],
+            "sh_size":[3048, "0x00000be8"],
+            "sh_link":[35, "0x00000023"],
+            "sh_info":[21, "0x00000015"],
+            "sh_addralign":[8, "0x00000008"],
+            "sh_entsize":[24, "0x00000018"],
+            "data_list":
+            [
+                {
+                    "d_type":[11, "ELF_T_SYM", "Symbol record."],
+                    "d_size":[3048, "0x00000be8"],
+                    "d_off":[0, "0x00000000"],
+                    "d_align":[8, "0x00000008"],
+                    "symbol_list":
+                    [
+
+                        {
+                            "st_name": "get_section_by_address",
+                            "st_value":[22129, "0x00005671"],
+                            "st_size":[324, "0x00000144"],
+                            "st_shndx":[16, "0x00000010"],
+                            "st_info":[18, "0x00000012"],
+                            "ST_BIND":[1, "STB_GLOBAL", "Global symbol"],
+                            "ST_TYPE":[2, "STT_FUNC", "Symbol is a code object"],
+                            "st_other":[0, "0x00000000"],
+                            "ST_VISIBILITY":[0, "STV_DEFAULT", "Default symbol visibility rules"],
+                            "obj_crc":[2219958429, "0x8451e09d"],
+                            "obj_data":[243,15,30,250,85,72,137,229,72,131,196,128,72,137,125,136,72,137,117,128,100,72,139,4,37,40,0,0,0,72,137,69,248,49,192,72,199,69,152,0,0,0,0,72,199,69,168,0,0,0,0,72,199,69,168,0,0,0,0,233,182,0,0,0]
+                        },
+
+*/
